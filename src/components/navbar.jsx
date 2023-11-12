7de2582cb3b9c38713c910f96ff3a7a16b1a8340
kuetche alex <kuetchealex99@gmail.com>
2023-11-12 09:15:25 +0100

    deuxieme commit

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -141,7 +141,7 @@ function Navbar() {
               <a href="#">Tendances</a>
             </li>
             <li>
-              <a href="#">Les plus regardés aujourd'hui</a>
+              <a href="#">Les plus regardés </a>
             </li>
           </ul>
         </div>
